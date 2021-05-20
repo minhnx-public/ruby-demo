61bc7699af71f6aac7c0332a669163cb65dd983e
minhnx2 <minhnx2@fpt.com.vn>
2021-05-20 09:05:38 +0700

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
   resources :microposts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index"
@@ -6,6 +8,4 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-
-  resources :microposts
 end
